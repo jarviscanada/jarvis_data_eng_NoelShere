a78fc0cd5a393fa83f00ef87f8d1155c80454a38
Noel Shere <noelshere60@gmail.com>
2022-03-23 20:51:41 +0000

    HOST_Usage

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+meminfo=$(cat /proc/meminfo)
+diskii=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+#Current time in YYYY-mm-dd HH:MM:SS format
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+
+#usage
+memory_free=$(echo "$meminfo"  | egrep "^MemFree:" | awk '{print $2}' | xargs)
+cpu_idle=$(echo "$meminfo"  | egrep "^Inactive:" | awk '{print $2}' | xargs)
+cpu_kernel=$(echo "$meminfo"  | egrep "^KernelStack:" | awk '{print $2}' | xargs)
+disk_io=$(vmstat -d | awk 'NR==3 {print $10}' | xargs)
+disk_available=$(echo "$diskii" | awk 'NR==3 {print $4}' | xargs)
+
+#test
+echo $disk_io
+echo $memory_free
+echo $cpu_idle
+echo $cpu_kernel
+echo $disk_available
+
+exit 1
