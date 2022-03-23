798276176d7074db1ab79cc3116d872117266142
Noel Shere <noelshere60@gmail.com>
2022-03-23 19:45:04 +0000

    add

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+lscpu_out=`lscpu`
+meminfo=$(cat /proc/meminfo)
+
+hostname=$(hostname -f)
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(echo "$meminfo"  | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
