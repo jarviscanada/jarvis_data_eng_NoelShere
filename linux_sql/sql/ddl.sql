a8dd6670807410840a2934ccd5f8c88111553732
Noel Shere <noelshere60@gmail.com>
2022-03-24 06:45:54 +0000

    changes

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXITS PUBLIC.host_info
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
 (
     "timestamp" TIMESTAMP NOT NULL,
-    host_id SERIAL NOT NULL REFERENCES host_info(id),
+    host_id SERIAL NOT NULL REFERENCES,
     memory_free INTEGER NOT NULL,
     cpu_idle INTEGER NOT NULL,
     cpu_kernel INTEGER,
